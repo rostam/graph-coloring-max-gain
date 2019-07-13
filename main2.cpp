b0b2250d8cf635db2ebdca9bd2692bb9e723704e
Ali <rostamiev@gmail.com>
2019-07-13 17:44:37 +0200

    Improving the code

--- main2.cpp
+++ main2.cpp
@@ -10,7 +10,8 @@
 #include <omp.h>
 #include <chrono>
 
-std::tuple<int, int, int, int> compute_discovered_misses(const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m, int color);
+std::tuple<int, int, int, int>
+compute_discovered_misses(const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m, int color);
 
 using std::cout;
 using std::cerr;
@@ -36,7 +37,8 @@ graph matrix2graph_limited(const boost::numeric::ublas::matrix<int> &m, int kk) 
             if (E_missed != 0) edges.emplace_back(i, j, weight);
         }
     }
-    sort(begin(edges), end(edges), [&](std::tuple<int, int, int> t1, std::tuple<int, int, int> t2) {return get<2>(t1) > get<2>(t2);});
+    sort(begin(edges), end(edges),
+         [&](std::tuple<int, int, int> t1, std::tuple<int, int, int> t2) { return get<2>(t1) > get<2>(t2); });
     for (int i = kk; i < edges.size(); i++) {
         auto[v1, v2, w] = edges[i];
         g.add_edge(v1, v2, w);
@@ -46,9 +48,9 @@ graph matrix2graph_limited(const boost::numeric::ublas::matrix<int> &m, int kk) 
 }
 
 std::tuple<int, int, int> get_bounds(int num_colors_natural_full) {
-    if(num_colors_natural_full > 220) {
+    if (num_colors_natural_full > 220) {
         return {num_colors_natural_full - 200, num_colors_natural_full, 10};
-    } else if(num_colors_natural_full > 120) {
+    } else if (num_colors_natural_full > 120) {
         return {num_colors_natural_full - 100, num_colors_natural_full, 10};
     } else if (num_colors_natural_full > 35) {
         return {num_colors_natural_full - 30, num_colors_natural_full, 3};
@@ -62,28 +64,21 @@ std::tuple<int, int, int> get_bounds(int num_colors_natural_full) {
 int main(int argc, const char *argv[]) {
     using boost::numeric::ublas::matrix;
     using boost::numeric::ublas::matrix_column;
-//    auto matrix_arr = {"nos3.mtx", "plbuckle.mtx", "bcsstk08.mtx", "bcsstk08", "1138_bus.mtx", "G51.mtx", "bcsstm13.mtx", "gemat11.mtx"};
+//    auto matrix_arr = {"mats/nos3.mtx", "mats/plbuckle.mtx", "mats/bcsstk08.mtx", "mats/bcsstk09.mtx", "mats/G51.mtx", "mats/bcsstm13.mtx", "mats/gemat11.mtx"};
     auto matrix_arr = {"mats/bcsstk08.mtx"};
-    int sample = 30;
-    std::ofstream out2(std::string("mats/res/bcsstk08_") + "k.csv");
-    out2 << "k,mnat,mnew,mlfo,msat" << endl;
-
+    std::ofstream out(std::string("bcsstk08_res.csv"));
+    out << "numOfColor,mnat,mnew,mlfo,msat,k,ck" << endl;
     auto start = std::chrono::steady_clock::now();
-#pragma omp parallel for
-    for (int k = 0; k < 15; k+=1) {
-//        std::cout << "k = " << k << std::endl;
-        for (auto matrix_name : matrix_arr) {
-//            std::cout << matrix_name << " " << std::endl;
-            matrix_market mm(matrix_name);
-            matrix<int> m = mm.to_ublas_matrix();
+    for (auto matrix_name : matrix_arr) {
+        matrix_market mm(matrix_name);
+        matrix<int> m = mm.to_ublas_matrix();
+        graph g = matrix2graph_limited(m, 0);
+        auto[num_colors_natural_full, color_vec_natural_full] = g.greedy_color(1000000);
+        auto[from, to, step] = get_bounds(num_colors_natural_full);
+//#pragma omp parallel for
+        for (int k = 0; k < 100; k += 10) {
             graph g = matrix2graph_limited(m, k);
             auto[num_colors_natural_full, color_vec_natural_full] = g.greedy_color(1000000);
-//            cout << "num of colors of full coloring: " << num_colors_natural_full << endl;
-//            if (num_colors_natural_full <= 10) return 10;
-            auto[from, to, step] = get_bounds(num_colors_natural_full);
-//            std::cout << from << " " << to << " " << step << std::endl;
-            std::ofstream out(std::string(matrix_name) + std::to_string(k) + ".csv");
-            out << "numOfColor,mnat,mnew,mlfo,msat" << endl;
             auto[num_colors_natural, color_vec_natural] = g.greedy_color(100000);
             std::vector<int> ord = g.optimum_order();
             auto[num_colors_newIdea, color_vec_newIdea] = g.greedy_color_limited(ord, 100000);
@@ -91,36 +86,39 @@ int main(int argc, const char *argv[]) {
             auto[num_colors_lfo, color_vec_lfo] = g.greedy_color_limited(lfo_ord, 100000);
             auto[num_colors_sat, color_vec_sat] = g.saturation_degree_ordering_coloring(100000);
             for (int color = from; color <= to; color += step) {
-                auto[all_sum_nat, all_discovered_color_zero_sum_nat, all_misses_nat, all_misses_color_zero_sum_nat] = compute_discovered_misses(color_vec_natural, m, color);
-                auto[all_sum_new, all_discovered_color_zero_sum_new, all_misses_new, all_misses_color_zero_sum_new] = compute_discovered_misses(color_vec_newIdea, m, color);
-                auto[all_sum_lfo, all_discovered_color_zero_sum_lfo, all_misses_lfo, all_misses_color_zero_sum_lfo] = compute_discovered_misses(color_vec_lfo, m, color);
-                auto[all_sum_sat, all_discovered_color_zero_sum_sat, all_misses_sat, all_misses_color_zero_sum_sat] = compute_discovered_misses(color_vec_sat, m, color);
-                int all_discovered_natural = all_sum_nat + all_discovered_color_zero_sum_nat;//compute_discovered(color_vec_natural, m, color);
-                int all_discovered_newIdea = all_sum_new + all_discovered_color_zero_sum_new;//compute_discovered(color_vec_newIdea, m, color);
-                int all_discovered_lfo = all_sum_lfo + all_discovered_color_zero_sum_lfo;//compute_discovered(color_vec_lfo, m, color);
-                int all_discovered_sat = all_sum_sat + all_discovered_color_zero_sum_sat;//compute_discovered(color_vec_sat, m, color);
+                auto[all_sum_nat, all_discovered_color_zero_sum_nat, all_misses_nat, all_misses_color_zero_sum_nat] = compute_discovered_misses(
+                        color_vec_natural, m, color);
+                auto[all_sum_new, all_discovered_color_zero_sum_new, all_misses_new, all_misses_color_zero_sum_new] = compute_discovered_misses(
+                        color_vec_newIdea, m, color);
+                auto[all_sum_lfo, all_discovered_color_zero_sum_lfo, all_misses_lfo, all_misses_color_zero_sum_lfo] = compute_discovered_misses(
+                        color_vec_lfo, m, color);
+                auto[all_sum_sat, all_discovered_color_zero_sum_sat, all_misses_sat, all_misses_color_zero_sum_sat] = compute_discovered_misses(
+                        color_vec_sat, m, color);
+                int all_discovered_natural = all_sum_nat +
+                                             all_discovered_color_zero_sum_nat;//compute_discovered(color_vec_natural, m, color);
+                int all_discovered_newIdea = all_sum_new +
+                                             all_discovered_color_zero_sum_new;//compute_discovered(color_vec_newIdea, m, color);
+                int all_discovered_lfo =
+                        all_sum_lfo + all_discovered_color_zero_sum_lfo;//compute_discovered(color_vec_lfo, m, color);
+                int all_discovered_sat =
+                        all_sum_sat + all_discovered_color_zero_sum_sat;//compute_discovered(color_vec_sat, m, color);
                 out << color << "," << all_discovered_natural << "," << all_discovered_newIdea << ","
-                    << all_discovered_lfo << "," << all_discovered_sat << endl;
-//                if(color == sample) {
-                    out2 << k << "," << all_discovered_natural << "," << all_discovered_newIdea << ","
-                         << all_discovered_lfo << "," << all_discovered_sat << endl;
-//                    break;
-//                }
+                    << all_discovered_lfo << "," << all_discovered_sat << "," << k << "," << num_colors_natural_full
+                    << endl;
             }
-            out.flush();
-            out.close();
         }
     }
     auto end = std::chrono::steady_clock::now();
     cerr << "Elapsed time in milliseconds for the main loop: "
          << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
          << " ms" << endl;
-    out2.flush();
-    out2.close();
+    out.flush();
+    out.close();
     return 0;
 }
 
-std::tuple<int, int, int, int> compute_discovered_misses(const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m, int color) {
+std::tuple<int, int, int, int>
+compute_discovered_misses(const std::vector<int> &color_vec, boost::numeric::ublas::matrix<int> &m, int color) {
     auto start = std::chrono::steady_clock::now();
     std::vector<boost::numeric::ublas::vector<int>> discovered(color);
     boost::numeric::ublas::vector<int> zeros = boost::numeric::ublas::zero_vector<int>(m.size2());
@@ -160,5 +158,5 @@ std::tuple<int, int, int, int> compute_discovered_misses(const std::vector<int> 
 //    cerr << "Elapsed time in milliseconds : "
 //         << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
 //         << " ms" << endl;
-    return {all_sum, all_discovered_color_zero_sum, all_misses,all_misses_color_zero_sum};
+    return {all_sum, all_discovered_color_zero_sum, all_misses, all_misses_color_zero_sum};
 }
